2b84843da64401dcbc840c0a2b7449e8ea504207
Dagger <inankai@sina.com>
2018-04-17 18:12:07 +0800

    测试gradle依赖bintray仓库

--- app/src/main/java/com/shark/utils/MainActivity.java
+++ app/src/main/java/com/shark/utils/MainActivity.java
@@ -3,11 +3,14 @@ package com.shark.utils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.shark.utils.library.core.SLog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SLog.e(this,"SLog引用成功");
     }
 }
