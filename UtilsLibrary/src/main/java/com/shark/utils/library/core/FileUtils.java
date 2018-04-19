27a43502a4f9f2f2b1a663b645a6c5e79bd66cc9
Dagger <inankai@sina.com>
2018-04-19 11:22:02 +0800

    修复文件拷贝BUG

--- UtilsLibrary/src/main/java/com/shark/utils/library/core/FileUtils.java
+++ UtilsLibrary/src/main/java/com/shark/utils/library/core/FileUtils.java
@@ -2,38 +2,15 @@ package com.shark.utils.library.core;
 
 import android.content.Context;
 
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 
 public class FileUtils {
-    public static void copyToPrivateFolder(Context context, InputStream inputStream, String nameForSave) {
-        OutputStream outputStream = null;
-        try {
-            outputStream = context.openFileOutput(nameForSave, Context.MODE_PRIVATE);
-            byte[] cache = new byte[1024];
-            while (inputStream.read(cache) != -1) {
-                outputStream.write(cache);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (inputStream != null)
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            if (outputStream!=null){
-                try {
-                    outputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+    public static void copyFile(FileInputStream fileInputStream, FileOutputStream fileOutputStream) throws IOException {
+        FileChannel inputChannel = fileInputStream.getChannel();
+        FileChannel outputChannel = fileOutputStream.getChannel();
+        outputChannel.transferFrom(inputChannel, 0, inputChannel.size());
     }
 }
