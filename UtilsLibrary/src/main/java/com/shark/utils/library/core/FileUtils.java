25a12171a44f76f5fdce792413e4cdf6250a4635
Dagger <inankai@sina.com>
2018-04-19 14:31:44 +0800

    发布新版

--- UtilsLibrary/src/main/java/com/shark/utils/library/core/FileUtils.java
+++ UtilsLibrary/src/main/java/com/shark/utils/library/core/FileUtils.java
@@ -4,6 +4,8 @@ package com.shark.utils.library.core;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.channels.FileChannel;
 
 public class FileUtils {
@@ -11,5 +13,15 @@ public class FileUtils {
         FileChannel inputChannel = fileInputStream.getChannel();
         FileChannel outputChannel = fileOutputStream.getChannel();
         outputChannel.transferFrom(inputChannel, 0, inputChannel.size());
+    }
+
+    public static void copyFile(InputStream fileInputStream, OutputStream fileOutputStream) throws IOException {
+        int byteCount = 0;
+        byte[] buffer = new byte[1024];
+        while ((byteCount = fileInputStream.read(buffer)) > 0) {
+            fileOutputStream.write(buffer, 0, byteCount);
+        }
+        fileInputStream.close();
+        fileOutputStream.close();
     }
 }
